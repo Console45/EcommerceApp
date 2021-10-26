7f44703b298db89de40c056497362be6d1026094
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-26 16:41:40 +0000

    done with onboarding screen

--- screens/Onboarding.tsx
+++ screens/Onboarding.tsx
@@ -1,14 +1,40 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { FC, ReactElement } from "react";
-import { View, StyleSheet, SafeAreaView, StatusBar, Text } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  Image,
+  View,
+} from "react-native";
+import { Button } from "../components";
+import { NativeStackParamList, Screens } from "../navigations/screens";
 import { Colors, FontType, Metrics, Sizes } from "../theme";
+import { getScreenPercent } from "../utils";
 
-interface OnboardingProps {}
+interface OnboardingProps
+  extends NativeStackScreenProps<NativeStackParamList, Screens.ONBOARDING> {}
 
-export const Onboarding: FC<OnboardingProps> = ({}): ReactElement => {
+export const Onboarding: FC<OnboardingProps> = ({
+  navigation,
+}): ReactElement => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" />
       <Text style={styles.title}>Find your Gadget</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/onboard.png")}
+        />
+      </View>
+      <Button
+        style={styles.button}
+        onPress={() => navigation.navigate(Screens.LOGIN)}
+      >
+        <Text style={styles.buttonText}>Get started</Text>
+      </Button>
     </SafeAreaView>
   );
 };
@@ -19,10 +45,27 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.PRIMARY,
   },
   title: {
-    margin: Sizes.MAIN_PADDING,
+    marginHorizontal: Sizes.MAIN_PADDING,
+    marginTop: 30,
+    marginBottom: 40,
     color: Colors.WHITE,
-    fontSize: (16.7 / 100) * Metrics.SCREEN_WIDTH,
+    fontSize: getScreenPercent(16.7),
     fontFamily: FontType.RALEWAY_EXTRA_BOLD,
     lineHeight: 69,
+  },
+  imageContainer: {
+    width: Metrics.SCREEN_WIDTH,
+    alignItems: "center",
+    height: "60%",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  button: { backgroundColor: Colors.WHITE, width: "81%", alignSelf: "center" },
+  buttonText: {
+    color: Colors.PRIMARY,
+    fontFamily: FontType.RALEWAY_BOLD,
+    fontSize: getScreenPercent(5.5),
   },
 });
