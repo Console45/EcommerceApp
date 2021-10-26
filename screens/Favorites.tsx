a83979a35809df0ee71f0c60a54eec1202cd5c3d
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-26 12:47:25 +0000

    update

---
+++ screens/Favorites.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactElement } from "react";
+import { View, StyleSheet } from "react-native";
+
+interface FavouritesProps {}
+
+export const Favourites: FC<FavouritesProps> = ({}): ReactElement => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
