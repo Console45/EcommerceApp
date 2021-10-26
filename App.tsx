69d0938de2a6a0daa85fb6a943597b1e061c311f
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-26 12:01:12 +0000

    added login and oboarding screens to the native stack

--- App.tsx
+++ App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NativeStackParamList, Screens } from "./navigations";
-import { Home } from "./screens";
+import { Login, Onboarding } from "./screens";
 
 const Stack = createNativeStackNavigator<NativeStackParamList>();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={Screens.HOME}>
-        <Stack.Screen name={Screens.HOME} component={Home} />
+      <Stack.Navigator initialRouteName={Screens.ONBOARDING}>
+        <Stack.Screen name={Screens.ONBOARDING} component={Onboarding} />
+        <Stack.Screen name={Screens.LOGIN} component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- navigations/screens.ts
+++ navigations/screens.ts
@@ -1,6 +1,11 @@
 export enum Screens {
   HOME = "Home",
+  ONBOARDING = "Onboarding",
+  CART = "Cart",
+  LOGIN = "Login",
+  PRODUCTDETAIL = "ProductDetail",
 }
 export type NativeStackParamList = {
-  Home: undefined;
+  Onboarding: undefined;
+  Login: undefined;
 };
