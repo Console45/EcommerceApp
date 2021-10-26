11f0533f33af6649058ef76ac68e889867832350
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-26 13:42:13 +0000

    added custom font

--- App.tsx
+++ App.tsx
@@ -1,19 +1,39 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppLoading from "expo-app-loading";
 import { NativeStackParamList, Screens } from "./navigations/screens";
 import { Login, Onboarding } from "./screens";
 import { TabNavigator } from "./navigations/TabNavigator";
+import {
+  useFonts,
+  Raleway_800ExtraBold,
+  Raleway_700Bold,
+  Raleway_600SemiBold,
+  Raleway_400Regular,
+} from "@expo-google-fonts/raleway";
 
 const Stack = createNativeStackNavigator<NativeStackParamList>();
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={Screens.ONBOARDING}>
-        <Stack.Screen name={Screens.ONBOARDING} component={Onboarding} />
-        <Stack.Screen name={Screens.LOGIN} component={Login} />
-        <Stack.Screen name={Screens.SHOP} component={TabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  let [fontsLoaded] = useFonts({
+    Raleway_800ExtraBold,
+    Raleway_700Bold,
+    Raleway_600SemiBold,
+    Raleway_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={Screens.ONBOARDING}
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name={Screens.ONBOARDING} component={Onboarding} />
+          <Stack.Screen name={Screens.LOGIN} component={Login} />
+          <Stack.Screen name={Screens.SHOP} component={TabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
