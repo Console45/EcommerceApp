659b79694bce91570ba12a003116bf7d022f6a72
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-26 11:54:51 +0000

    added screens

---
+++ screens/Onboarding.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactElement } from "react";
+import { View, StyleSheet } from "react-native";
+
+interface OnboardingProps {}
+
+export const Onboarding: FC<OnboardingProps> = ({}): ReactElement => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
---
+++ screens/ProductDetail.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactElement } from "react";
+import { View, StyleSheet } from "react-native";
+
+interface ProductDetailProps {}
+
+export const ProductDetail: FC<ProductDetailProps> = ({}): ReactElement => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
