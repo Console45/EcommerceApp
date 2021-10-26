214d41acb2e7af626d03c4bf42772daf0121c60a
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-26 14:00:30 +0000

    added app theme

---
+++ theme/fonts.ts
@@ -0,0 +1,6 @@
+export enum FontType {
+  RALEWAY_EXTRA_BOLD = "Raleway_800ExtraBold",
+  RALEWAY_BOLD = "Raleway_700Bold",
+  RALEWAY_SEMIBOLD = "Raleway_600SemiBold",
+  RALEWAY_REGULAR = "Raleway_400Regular",
+}
---
+++ theme/metrics.ts
@@ -0,0 +1,7 @@
+import { Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+
+export enum Metrics {
+  SCREEN_WIDTH = width < height ? width : height,
+  SCREEN_HEIGHT = width < height ? height : width,
+}
