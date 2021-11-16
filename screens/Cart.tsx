9d68153b925a03453f0636303601d90460e8043c
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-11-16 21:36:53 +0000

    added default styling for cart favorites and History

--- screens/Cart.tsx
+++ screens/Cart.tsx
@@ -1,16 +1,64 @@
 import React, { FC, ReactElement } from "react";
-import { View, StyleSheet } from "react-native";
-import { Colors } from "../theme";
+import { View, StyleSheet, Image, Text } from "react-native";
+import { Button } from "../components";
+import { Colors, FontType } from "../theme";
+import { getScreenPercent } from "../utils";
+import { useNavigation } from "@react-navigation/core";
 
 interface CartProps {}
 
 export const Cart: FC<CartProps> = ({}): ReactElement => {
-  return <View style={styles.container}></View>;
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/images/nohistory.png")}
+      />
+      <Text style={styles.headText}>Your Cart is Empty</Text>
+      <Text style={styles.text}>
+        Hit the blue button down below to Create an order
+      </Text>
+      <Button
+        title="Start ordering"
+        style={styles.button}
+        textStyle={{ fontSize: getScreenPercent(4.2) }}
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.PRIMARY_BG,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    height: getScreenPercent(70),
+    width: getScreenPercent(70),
+  },
+  headText: {
+    fontFamily: FontType.RALEWAY_SEMIBOLD,
+    textAlign: "center",
+    fontSize: getScreenPercent(8),
+    marginTop: getScreenPercent(10),
+  },
+  text: {
+    width: getScreenPercent(54),
+    textAlign: "center",
+    marginTop: getScreenPercent(4.6),
+    fontFamily: FontType.RALEWAY_REGULAR,
+    fontSize: getScreenPercent(4.2),
+    lineHeight: getScreenPercent(6.1),
+  },
+  button: {
+    marginTop: getScreenPercent(7.4),
+    width: getScreenPercent(57.4),
+    height: getScreenPercent(12.8),
+    backgroundColor: Colors.SECONDARY,
   },
 });
--- screens/Favorites.tsx
+++ screens/Favorites.tsx
@@ -1,16 +1,64 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { FC, ReactElement } from "react";
-import { View, StyleSheet } from "react-native";
-import { Colors } from "../theme";
+import { View, StyleSheet, Image, Text } from "react-native";
+import { Button } from "../components";
+import { Colors, FontType } from "../theme";
+import { getScreenPercent } from "../utils";
 
 interface FavouritesProps {}
 
 export const Favourites: FC<FavouritesProps> = ({}): ReactElement => {
-  return <View style={styles.container}></View>;
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/images/nofav.png")}
+      />
+      <Text style={styles.headText}>No Favourites Yet</Text>
+      <Text style={styles.text}>
+        Hit the blue button down below to Create an order
+      </Text>
+      <Button
+        title="Start ordering"
+        style={styles.button}
+        textStyle={{ fontSize: getScreenPercent(4.2) }}
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.PRIMARY_BG,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    height: getScreenPercent(70),
+    width: getScreenPercent(70),
+  },
+  headText: {
+    fontFamily: FontType.RALEWAY_SEMIBOLD,
+    textAlign: "center",
+    fontSize: getScreenPercent(8),
+    marginTop: getScreenPercent(10),
+  },
+  text: {
+    width: getScreenPercent(54),
+    textAlign: "center",
+    marginTop: getScreenPercent(4.6),
+    fontFamily: FontType.RALEWAY_REGULAR,
+    fontSize: getScreenPercent(4.2),
+    lineHeight: getScreenPercent(6.1),
+  },
+  button: {
+    marginTop: getScreenPercent(7.4),
+    width: getScreenPercent(57.4),
+    height: getScreenPercent(12.8),
+    backgroundColor: Colors.SECONDARY,
   },
 });
